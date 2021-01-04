98c8473c3f1318dfb68b82919334ed3354460d37
Avery Dace <daceavery@gmail.com>
2021-01-03 23:33:13 -0600

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//assign var to input
+//assign var to button
+
+const itemBOX = document.getElementById("itemBox");
+const addHere =  document.getElementById("addHere");
+
+//add click function to button; save to local storage
+
+addHere.onclick = function (){
+const addBtn = addHere.value;  
+
+console.log(addHere);
+}
